b982d82d4cead44c8be8dcc1c73f12aa0e7ce59f
Dominik277 <dominikdoric277@gmail.com>
2021-01-05 21:00:32 +0100

    Dodan Tablayout za StapoviActivity.

--- app/src/main/java/fishing/tackle/shop/ui/stapovi/StapoviActivity.java
+++ app/src/main/java/fishing/tackle/shop/ui/stapovi/StapoviActivity.java
@@ -15,5 +15,8 @@ public class StapoviActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle("Stapovi");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
     }
 }

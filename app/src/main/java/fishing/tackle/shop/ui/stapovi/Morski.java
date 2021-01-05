aad9f1fc8aca56964bdaa3ea035061bbadfa4a25
Dominik277 <dominikdoric277@gmail.com>
2021-01-05 21:28:13 +0100

    Zavrsen TabLayout za Saranski stapovi activity.

---
+++ app/src/main/java/fishing/tackle/shop/ui/stapovi/Morski.java
@@ -0,0 +1,22 @@
+package fishing.tackle.shop.ui.stapovi;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import fishing.tackle.shop.R;
+
+public class Morski extends Fragment {
+
+    public Morski() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_morski, container, false);
+    }
+}

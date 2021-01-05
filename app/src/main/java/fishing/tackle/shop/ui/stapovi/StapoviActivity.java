e3fe38785ac08b2696b642fc3bdaa4d49000e158
Dominik277 <dominikdoric277@gmail.com>
2021-01-05 21:09:42 +0100

    Dodane reference i sav potreban kod u onCreate() metodu.

--- app/src/main/java/fishing/tackle/shop/ui/stapovi/StapoviActivity.java
+++ app/src/main/java/fishing/tackle/shop/ui/stapovi/StapoviActivity.java
@@ -1,22 +1,86 @@
 package fishing.tackle.shop.ui.stapovi;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 import fishing.tackle.shop.R;
 
 public class StapoviActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stapovi);
 
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Saranski"));
+        tabLayout.addTab(tabLayout.newTab().setText("Somovski"));
+        tabLayout.addTab(tabLayout.newTab().setText("Morski"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final MyAdapter adapter = new MyAdapter(this,getSupportFragmentManager(),
+                tabLayout.getTabCount());
+
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
         getSupportActionBar().setTitle("Stapovi");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
-
     }
+
+    class MyAdapter extends FragmentPagerAdapter{
+
+        Context context;
+        int totalTabs;
+
+        public MyAdapter(Context c,@NonNull FragmentManager fm,int totalTabs) {
+            super(fm);
+            context = c;
+            this.totalTabs = totalTabs;
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return totalTabs;
+        }
+    }
+
 }

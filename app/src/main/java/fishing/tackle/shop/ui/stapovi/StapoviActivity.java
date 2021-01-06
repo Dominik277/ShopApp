1175af04eb8456c736759d9a51d89ae56a4ad914
Dominik277 <dominikdoric277@gmail.com>
2021-01-06 16:38:20 +0100

    Implementirana POJO klasa Model.

--- app/src/main/java/fishing/tackle/shop/ui/stapovi/StapoviActivity.java
+++ app/src/main/java/fishing/tackle/shop/ui/stapovi/StapoviActivity.java
@@ -1,98 +1,32 @@
 package fishing.tackle.shop.ui.stapovi;
 
-import android.content.Context;
+import android.animation.ArgbEvaluator;
 import android.os.Bundle;
-import com.google.android.material.tabs.TabLayout;
-import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 import fishing.tackle.shop.R;
 
 public class StapoviActivity extends AppCompatActivity {
 
-    TabLayout tabLayout;
     ViewPager viewPager;
+    Adapter adapter;
+    List<Model> models;
+    Integer[] colors = null;
+    ArgbEvaluator argbEvaluator = new ArgbEvaluator();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stapovi);
 
-        tabLayout = findViewById(R.id.tabLayoutStapovi);
-        viewPager = findViewById(R.id.viewPagerStapovi);
 
-        tabLayout.addTab(tabLayout.newTab().setText("Saranski"));
-        tabLayout.addTab(tabLayout.newTab().setText("Somovski"));
-        tabLayout.addTab(tabLayout.newTab().setText("Morski"));
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-
-        final MyAdapter adapter = new MyAdapter(this,getSupportFragmentManager(),
-                tabLayout.getTabCount());
-
-        viewPager.setAdapter(adapter);
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-
-
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-        getSupportActionBar().setTitle("Stapovi");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        models = new ArrayList<>();
+        models
 
     }
-
-    class MyAdapter extends FragmentPagerAdapter{
-
-        Context context;
-        int totalTabs;
-
-        public MyAdapter(Context c,@NonNull FragmentManager fm,int totalTabs) {
-            super(fm);
-            context = c;
-            this.totalTabs = totalTabs;
-        }
-
-        @NonNull
-        @Override
-        public Fragment getItem(int position) {
-            switch (position){
-                case 0:
-                    Saranski saranskiFragment = new Saranski();
-                    return saranskiFragment;
-                case 1:
-                    Somovski somovskiFragment = new Somovski();
-                    return somovskiFragment;
-                case 2:
-                    Morski morskiFragment = new Morski();
-                    return morskiFragment;
-                default:
-                    return null;
-            }
-        }
-
-        @Override
-        public int getCount() {
-            return totalTabs;
-        }
-    }
-
 }

b5f08be409c529c6f97fe4550661da8628072044
Dominik277 <dominikdoric277@gmail.com>
2021-01-06 19:05:15 +0100

    Dodana nova slika za svaki artikal.

--- app/src/main/java/fishing/tackle/shop/ui/stapovi/StapoviActivity.java
+++ app/src/main/java/fishing/tackle/shop/ui/stapovi/StapoviActivity.java
@@ -27,19 +27,19 @@ public class StapoviActivity extends AppCompatActivity {
 
         models = new ArrayList<>();
         models.add(new Model(R.drawable.shimanotribal,"Shimano Tribal TX-9A 13ft Intensity","Cijena: 3172,43 kn",R.string.opisShimanoTribal));
-        models.add(new Model(R.drawable.shimanotribal,"Century FMA-2 13ft 3-5oz","Cijena: 2999,99 kn",R.string.opisCenturyFMA));
-        models.add(new Model(R.drawable.shimanotribal,"Free Spirit HI-S ive 13ft","Cijena: 4227,43 kn",R.string.opisFreeSpiritHI_S_ive));
-        models.add(new Model(R.drawable.shimanotribal,"Fox Horizon X5 13ft","Cijena: 1903,75 kn",R.string.opisFoxHorizon));
-        models.add(new Model(R.drawable.shimanotribal,"Greys Xlerate 13ft 3.5lb","Cijena: 2772,10 kn",R.string.opisGreysXlerate));
-        models.add(new Model(R.drawable.shimanotribal,"Nash NR Toro 13ft","Cijena: 2280,10 kn",R.string.opisNashNRTORO));
-        models.add(new Model(R.drawable.shimanotribal,"Century ADV-1 13ft","Cijena: 2143,40 kn",R.string.opisCenturyADV));
-        models.add(new Model(R.drawable.shimanotribal,"Prologic Quasar K1 13ft","Cijena: 1679,40 kn",R.string.opisPrologicQuasar));
-        models.add(new Model(R.drawable.shimanotribal,"Daiwa Infinity DF 13ft","Cijena: 1679,40 kn",R.string.opisDaiwaInfinity));
-        models.add(new Model(R.drawable.shimanotribal,"Sportex Catapult 13ft","Cijena: 1679,40 kn",R.string.opisSportexCatapult));
-        models.add(new Model(R.drawable.shimanotribal,"Dawia Longbow 13ft","Cijena: 1679,40 kn",R.string.opisDaiwaLongbow));
-        models.add(new Model(R.drawable.shimanotribal,"JRC Contact 13ft","Cijena: 1679,40 kn",R.string.opisJRCContact));
-        models.add(new Model(R.drawable.shimanotribal,"Nash H-Gun 13ft","Cijena: 1679,40 kn",R.string.opisNashHGun));
-        models.add(new Model(R.drawable.shimanotribal,"Shimano Alivio 13ft","Cijena: 1679,40 kn",R.string.opisShimanoAlivio));
+        models.add(new Model(R.drawable.centuryfma,"Century FMA-2 13ft 3-5oz","Cijena: 2999,99 kn",R.string.opisCenturyFMA));
+        models.add(new Model(R.drawable.freespirithisive,"Free Spirit HI-S ive 13ft","Cijena: 4227,43 kn",R.string.opisFreeSpiritHI_S_ive));
+        models.add(new Model(R.drawable.foxhorizonx5,"Fox Horizon X5 13ft","Cijena: 1903,75 kn",R.string.opisFoxHorizon));
+        models.add(new Model(R.drawable.greysxlerate,"Greys Xlerate 13ft 3.5lb","Cijena: 2772,10 kn",R.string.opisGreysXlerate));
+        models.add(new Model(R.drawable.nashnrtoro,"Nash NR Toro 13ft","Cijena: 2280,10 kn",R.string.opisNashNRTORO));
+        models.add(new Model(R.drawable.centuryadv,"Century ADV-1 13ft","Cijena: 2143,40 kn",R.string.opisCenturyADV));
+        models.add(new Model(R.drawable.prologicquasar,"Prologic Quasar K1 13ft","Cijena: 1679,40 kn",R.string.opisPrologicQuasar));
+        models.add(new Model(R.drawable.daiwainfinitydf,"Daiwa Infinity DF 13ft","Cijena: 1679,40 kn",R.string.opisDaiwaInfinity));
+        models.add(new Model(R.drawable.sportexcatapult,"Sportex Catapult 13ft","Cijena: 1679,40 kn",R.string.opisSportexCatapult));
+        models.add(new Model(R.drawable.dawialongbow,"Dawia Longbow 13ft","Cijena: 1679,40 kn",R.string.opisDaiwaLongbow));
+        models.add(new Model(R.drawable.jrccontact,"JRC Contact 13ft","Cijena: 1679,40 kn",R.string.opisJRCContact));
+        models.add(new Model(R.drawable.nashhgun,"Nash H-Gun 13ft","Cijena: 1679,40 kn",R.string.opisNashHGun));
+        models.add(new Model(R.drawable.shimanoalivio,"Shimano Alivio 13ft","Cijena: 1679,40 kn",R.string.opisShimanoAlivio));
 
         adapter = new Adapter(models,this);
 

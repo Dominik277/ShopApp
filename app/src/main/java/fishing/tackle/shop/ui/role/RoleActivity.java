4353bffa332b6be11a47ea99a6db6558e1aa3719
Dominik277 <dominikdoric277@gmail.com>
2021-01-07 11:56:50 +0100

    Implementiran izgled jednog cardview-a i adapter za njega.

--- app/src/main/java/fishing/tackle/shop/ui/role/RoleActivity.java
+++ app/src/main/java/fishing/tackle/shop/ui/role/RoleActivity.java
@@ -1,97 +1,24 @@
 package fishing.tackle.shop.ui.role;
 
-import android.content.Context;
-import android.os.Bundle;
-import com.google.android.material.tabs.TabLayout;
 
-import androidx.annotation.NonNull;
+import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 import fishing.tackle.shop.R;
 
 public class RoleActivity extends AppCompatActivity {
 
-    TabLayout tabLayout;
     ViewPager viewPager;
+    RoleAdapter roleAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_role);
 
-        tabLayout = findViewById(R.id.tabLayoutRole);
-        viewPager = findViewById(R.id.viewPagerRole);
-
-        tabLayout.addTab(tabLayout.newTab().setText("Saranske"));
-        tabLayout.addTab(tabLayout.newTab().setText("Somovske"));
-        tabLayout.addTab(tabLayout.newTab().setText("Morske"));
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-
-        final MyAdapter adapter = new MyAdapter(this,getSupportFragmentManager(),
-                tabLayout.getTabCount());
-
-        viewPager.setAdapter(adapter);
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
 
 
-        getSupportActionBar().setTitle("Role");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
-    class MyAdapter extends FragmentPagerAdapter{
-
-        Context context;
-        int totalTabs;
-
-        public MyAdapter(Context c, @NonNull FragmentManager fm,int totalTabs) {
-            super(fm);
-            context = c;
-            this.totalTabs = totalTabs;
-        }
-
-        @NonNull
-        @Override
-        public Fragment getItem(int position) {
-            switch (position){
-                case 0:
-                    Saranske saranskiFragment = new Saranske();
-                    return saranskiFragment;
-                case 1:
-                    Somovske somovskiFragment = new Somovske();
-                    return somovskiFragment;
-                case 2:
-                    Morske morskiFragment = new Morske();
-                    return morskiFragment;
-                default:
-                    return null;
-            }
-        }
-
-        @Override
-        public int getCount() {
-            return totalTabs;
-        }
     }
 
 }
--- app/src/main/java/fishing/tackle/shop/ui/role/RoleAdapter.java
+++ app/src/main/java/fishing/tackle/shop/ui/role/RoleAdapter.java
@@ -1,4 +1,68 @@
 package fishing.tackle.shop.ui.role;
 
-public class RoleAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import fishing.tackle.shop.R;
+
+public class RoleAdapter extends PagerAdapter {
+
+    private List<RoleModel> roleModels;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+    public RoleAdapter(List<RoleModel> roleModels,Context context){
+        this.roleModels = roleModels;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return roleModels.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view.equals(object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, final int position){
+        layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.item_role,container,false);
+
+        ImageView imageView;
+        TextView title;
+        TextView desc;
+
+        imageView = view.findViewById(R.id.imageRole);
+        title = view.findViewById(R.id.titleRole);
+        desc = view.findViewById(R.id.descRole);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context,RoleDetailActivity.class);
+                intent.putExtra("param",roleModels.get(position).getTitle());
+                context.startActivity(intent);
+            }
+        });
+        container.addView(view,0);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View)object);
+    }
 }

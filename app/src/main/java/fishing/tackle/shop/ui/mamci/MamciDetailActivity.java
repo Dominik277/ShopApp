9e5b10a6d9dc75cb6fc0f717d8b038d51e1f4815
Dominik277 <dominikdoric277@gmail.com>
2021-01-07 23:20:42 +0100

    Kreiran izgled jednog cardView-a.

--- app/src/main/java/fishing/tackle/shop/ui/mamci/MamciDetailActivity.java
+++ app/src/main/java/fishing/tackle/shop/ui/mamci/MamciDetailActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import fishing.tackle.shop.R;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MamciDetailActivity extends AppCompatActivity {
 
@@ -11,5 +12,8 @@ public class MamciDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_mamci);
+
+        TextView textView = findViewById(R.id.textViewMamci);
+        textView.setText(getIntent().getStringExtra("param"));
     }
 }

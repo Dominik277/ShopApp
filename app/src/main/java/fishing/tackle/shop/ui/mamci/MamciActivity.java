e74788c2b4b98c497d2b6d97e1c783ebf1e07657
Dominik277 <dominikdoric277@gmail.com>
2021-01-07 23:36:53 +0100

    Kreiran adapter za mamce.

--- app/src/main/java/fishing/tackle/shop/ui/mamci/MamciActivity.java
+++ app/src/main/java/fishing/tackle/shop/ui/mamci/MamciActivity.java
@@ -1,97 +1,18 @@
 package fishing.tackle.shop.ui.mamci;
 
-import android.content.Context;
+
 import android.os.Bundle;
-
-import com.google.android.material.tabs.TabLayout;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
 import fishing.tackle.shop.R;
 
 public class MamciActivity extends AppCompatActivity {
 
-    TabLayout tabLayout;
-    ViewPager viewPager;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mamci);
 
-        tabLayout = findViewById(R.id.tabLayoutMamci);
-        viewPager = findViewById(R.id.viewPagerMamci);
 
-        tabLayout.addTab(tabLayout.newTab().setText("Boile"));
-        tabLayout.addTab(tabLayout.newTab().setText("Kukuruz"));
-        tabLayout.addTab(tabLayout.newTab().setText("Pelete"));
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-
-        final MyAdapter adapter = new MyAdapter(this,getSupportFragmentManager(),
-                tabLayout.getTabCount());
-
-        viewPager.setAdapter(adapter);
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-        getSupportActionBar().setTitle("Mamci");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
-    public class MyAdapter extends FragmentPagerAdapter {
-
-        Context context;
-        int totalTabs;
-
-        public MyAdapter(Context c, @NonNull FragmentManager fm, int totalTabs) {
-            super(fm);
-            context = c;
-            this.totalTabs = totalTabs;
-        }
-
-        @NonNull
-        @Override
-        public Fragment getItem(int position) {
-            switch (position){
-                case 0:
-                    Boile boileFragment = new Boile();
-                    return boileFragment;
-                case 1:
-                    Kukuruz kukuruzFragment = new Kukuruz();
-                    return kukuruzFragment;
-                case 2:
-                    Pelete peleteFragment = new Pelete();
-                    return peleteFragment;
-                default:
-                    return null;
-            }
-        }
-
-        @Override
-        public int getCount() {
-            return totalTabs;
-        }
-    }
-
 }
--- app/src/main/java/fishing/tackle/shop/ui/mamci/MamciAdapter.java
+++ app/src/main/java/fishing/tackle/shop/ui/mamci/MamciAdapter.java
@@ -1,4 +1,67 @@
 package fishing.tackle.shop.ui.mamci;
 
-public class MamciAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.List;
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import fishing.tackle.shop.R;
+
+public class MamciAdapter extends PagerAdapter {
+
+    private List<MamciModel> mamciModels;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+    public MamciAdapter(List<MamciModel> mamciModels, Context context) {
+        this.mamciModels = mamciModels;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return mamciModels.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view.equals(object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, final int position) {
+        layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.item_mamci, container, false);
+
+        ImageView imageView;
+        TextView title, desc;
+
+        imageView = view.findViewById(R.id.image);
+        title = view.findViewById(R.id.title);
+        desc = view.findViewById(R.id.desc);
+
+        imageView.setImageResource(mamciModels.get(position).getImage());
+        title.setText(mamciModels.get(position).getTitle());
+        desc.setText(mamciModels.get(position).getDesc());
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, MamciDetailActivity.class);
+                intent.putExtra("param", mamciModels.get(position).getTitle());
+                context.startActivity(intent);
+                // finish();
+            }
+        });
+
+        container.addView(view, 0);
+        return view;
+    }
+
 }

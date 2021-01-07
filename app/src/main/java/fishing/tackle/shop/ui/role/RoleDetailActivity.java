5c33ceca8669b8fb1eca28c48c169775fcf99c55
Dominik277 <dominikdoric277@gmail.com>
2021-01-07 12:08:17 +0100

    Dodan izgled za activity_role.xml.

--- app/src/main/java/fishing/tackle/shop/ui/role/RoleDetailActivity.java
+++ app/src/main/java/fishing/tackle/shop/ui/role/RoleDetailActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import fishing.tackle.shop.R;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RoleDetailActivity extends AppCompatActivity {
 
@@ -11,5 +12,8 @@ public class RoleDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_role);
+
+        TextView textView = findViewById(R.id.textViewRole);
+        textView.setText(getIntent().getStringExtra("param"));
     }
 }

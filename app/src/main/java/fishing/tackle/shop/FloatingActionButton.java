eb05a82df2edc5c463e1b9f0eb0b4ad107de198f
Dominik277 <dominikdoric277@gmail.com>
2021-01-09 15:10:56 +0100

    Implementiran vizualni dio za upit koji ce se pokretati prilikom klika na FloatingActionButton.

---
+++ app/src/main/java/fishing/tackle/shop/FloatingActionButton.java
@@ -0,0 +1,14 @@
+package fishing.tackle.shop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FloatingActionButton extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_floating_action_button);
+    }
+}

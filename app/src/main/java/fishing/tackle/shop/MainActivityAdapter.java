8c72e7c512a0fa01e52e6e6382a7d543e425a62f
Dominik277 <dominikdoric277@gmail.com>
2021-01-09 02:22:47 +0100

    Ponovno napravljena klasa model i napravljen adapter za MainActivity.

--- app/src/main/java/fishing/tackle/shop/MainActivityAdapter.java
+++ app/src/main/java/fishing/tackle/shop/MainActivityAdapter.java
@@ -1,4 +1,66 @@
 package fishing.tackle.shop;
 
-public class MainActivityAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import fishing.tackle.shop.ui.stapovi.StapoviModel;
+
+public class MainActivityAdapter extends PagerAdapter {
+
+    private List<MainActivityModel> modelsMain;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+    @Override
+    public int getCount() {
+        return modelsMain.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view.equals(object);
+    }
+
+    public Object instantiateItem(ViewGroup container, final int position){
+        layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.item_main_gornji_i_donji,container,false);
+
+        ImageView imageMainGore,imageMainDolje;
+        TextView titleMainGore,titleMainDolje,descMainGore,descMainDolje;
+
+        imageMainGore = view.findViewById(R.id.imageAkcijaGore);
+        imageMainDolje = view.findViewById(R.id.imageAkcijaDonji);
+
+        titleMainGore = view.findViewById(R.id.opisAkcijaGore);
+        titleMainDolje = view.findViewById(R.id.opisAkcijaDonji);
+
+        descMainGore = view.findViewById(R.id.cijenaAkcijaGore);
+        descMainDolje = view.findViewById(R.id.cijenaAkcijaDonji);
+
+        imageMainGore.setImageResource(modelsMain.get(position).getImageMainGore());
+        imageMainDolje.setImageResource(modelsMain.get(position).getImageMainDolje());
+
+        titleMainGore.setText(modelsMain.get(position).getTitleMainGore());
+        titleMainDolje.setText(modelsMain.get(position).getTitleMainDolje());
+
+        descMainGore.setText(modelsMain.get(position).getDescMainGore());
+        descMainDolje.setText(modelsMain.get(position).getDescMainDolje());
+
+        container.addView(view,0);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View)object);
+    }
+
 }
--- app/src/main/java/fishing/tackle/shop/MainActivityModel.java
+++ app/src/main/java/fishing/tackle/shop/MainActivityModel.java
@@ -2,37 +2,67 @@ package fishing.tackle.shop;
 
 public class MainActivityModel {
 
-    private int imageMain;
-    private String titleMain;
-    private String descMain;
+    private int imageMainGore;
+    private int imageMainDolje;
+    private String titleMainGore;
+    private String titleMainDolje;
+    private String descMainGore;
+    private String descMainDolje;
 
-    public MainActivityModel(int imageMain, String titleMain, String descMain) {
-        this.imageMain = imageMain;
-        this.titleMain = titleMain;
-        this.descMain = descMain;
+    public MainActivityModel(int imageMainGore, int imageMainDolje, String titleMainGore, String titleMainDolje, String descMainGore, String descMainDolje) {
+        this.imageMainGore = imageMainGore;
+        this.imageMainDolje = imageMainDolje;
+        this.titleMainGore = titleMainGore;
+        this.titleMainDolje = titleMainDolje;
+        this.descMainGore = descMainGore;
+        this.descMainDolje = descMainDolje;
     }
 
-    public int getImageMain() {
-        return imageMain;
+    public int getImageMainGore() {
+        return imageMainGore;
     }
 
-    public void setImageMain(int imageMain) {
-        this.imageMain = imageMain;
+    public void setImageMainGore(int imageMainGore) {
+        this.imageMainGore = imageMainGore;
     }
 
-    public String getTitleMain() {
-        return titleMain;
+    public int getImageMainDolje() {
+        return imageMainDolje;
     }
 
-    public void setTitleMain(String titleMain) {
-        this.titleMain = titleMain;
+    public void setImageMainDolje(int imageMainDolje) {
+        this.imageMainDolje = imageMainDolje;
     }
 
-    public String getDescMain() {
-        return descMain;
+    public String getTitleMainGore() {
+        return titleMainGore;
     }
 
-    public void setDescMain(String descMain) {
-        this.descMain = descMain;
+    public void setTitleMainGore(String titleMainGore) {
+        this.titleMainGore = titleMainGore;
+    }
+
+    public String getTitleMainDolje() {
+        return titleMainDolje;
+    }
+
+    public void setTitleMainDolje(String titleMainDolje) {
+        this.titleMainDolje = titleMainDolje;
+    }
+
+    public String getDescMainGore() {
+        return descMainGore;
+    }
+
+    public void setDescMainGore(String descMainGore) {
+        this.descMainGore = descMainGore;
+    }
+
+    public String getDescMainDolje() {
+        return descMainDolje;
+    }
+
+    public void setDescMainDolje(String descMainDolje) {
+        this.descMainDolje = descMainDolje;
     }
 }
